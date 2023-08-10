19ac40ecab44c77ed5f93db20a0d1cfcd778c207
hou27 <ataj125@gmail.com>
2023-08-10 21:41:10 +0900

    RNN: 실제 값으로 되돌리기 위해 actual values 따로 저장

--- RNN/forward_pass.py
+++ RNN/forward_pass.py
@@ -6,25 +6,38 @@ import numpy as np
 from activation.softmax import softmax
 from activation.tanh import tanh
 
+
 # Vanilla RNN forward pass
 def forward_pass(inputs, hidden_state, params):
     # 파라미터 추출
-    W_xh, W_hh, W_hy, b_hidden, b_out = params # 입력 가중치 행렬, hidden 가중치 행렬, 출력 가중치 행렬, hidden bias, 출력 bias
-    
+    (
+        W_xh,
+        W_hh,
+        W_hy,
+        b_hidden,
+        b_out,
+    ) = params  # 입력 가중치 행렬, hidden 가중치 행렬, 출력 가중치 행렬, hidden bias, 출력 bias
+
     # outputs, hidden_states 초기화
     outputs, hidden_states = [], []
-    
+    # actual_values 초기화
+    actual_values = []
+
     # For each element in input sequence
     for t in range(len(inputs)):
-
         # hidden_state 계산
-        hidden_state = tanh(np.dot(W_hh, hidden_state) + np.dot(W_xh, inputs[t]) + b_hidden)
+        hidden_state = tanh(
+            np.dot(W_hh, hidden_state) + np.dot(W_xh, inputs[t]) + b_hidden
+        )
 
         # output 계산
-        out = softmax(np.dot(W_hy, hidden_state) + b_out)
-        
+        values = np.dot(W_hy, hidden_state) + b_out
+        # out = softmax(np.dot(W_hy, hidden_state) + b_out)
+        out = softmax(values)
+
         # 저장
         outputs.append(out)
+        actual_values.append(values)
         hidden_states.append(hidden_state.copy())
-    
-    return outputs, hidden_states
+
+    return outputs, hidden_states, actual_values
