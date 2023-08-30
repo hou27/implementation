01be5bf66d25ce3ef6b1cfec98d50a09c68adbb6
hou27 <ataj125@gmail.com>
2023-08-31 01:35:52 +0900

    RNN: 변경사항 반영 및 차원 조절 - #2

--- RNN/main.py
+++ RNN/main.py
@@ -6,8 +6,8 @@ from forward_pass import forward_pass
 from init import init_rnn
 from update_params import update_params
 
-hidden_size = 50
-input_dims = 5
+hidden_size = 1
+input_dims = 1
 
 if __name__ == "__main__":
     X = np.array(
@@ -21,7 +21,7 @@ if __name__ == "__main__":
     )
     y = np.array([[6.0], [7.0], [8.0], [9.0], [10.0]])
 
-    num_epochs = 55
+    num_epochs = 1000
     learning_rate = 3e-4
 
     # 네트워크 초기화
@@ -39,14 +39,16 @@ if __name__ == "__main__":
         epoch_training_loss = 0
         epoch_validation_loss = 0
 
-        inputs = X
-        targets = y
+        # inputs = X
+        # targets = y
+        inputs = np.array([1, 2, 3, 4])
+        targets = np.array([2, 3, 4, 5])
 
         # 매 epoch마다 hidden state를 0으로 초기화
         hidden_state = np.zeros_like(hidden_state)
 
         # Forward pass
-        outputs, hidden_states, _ = forward_pass(inputs, hidden_state, params)
+        outputs, hidden_states = forward_pass(inputs, hidden_state, params)
 
         # Backward pass
         loss, grads = backward_pass(inputs, outputs, hidden_states, targets, params)
@@ -63,29 +65,18 @@ if __name__ == "__main__":
         # Loss 출력
         print(f"Epoch {i}, training loss: {training_loss[-1]}")
 
-    # Get first sentence in test set
     inputs = np.array(
-        [
-            [[1.0], [2.0], [3.0], [4.0], [5.0]],
-            [[2.0], [3.0], [4.0], [5.0], [6.0]],
-            [[3.0], [4.0], [5.0], [6.0], [7.0]],
-            [[4.0], [5.0], [6.0], [7.0], [8.0]],
-            [[5.0], [6.0], [7.0], [8.0], [9.0]],
-            [[6.0], [7.0], [8.0], [9.0], [10.0]],
-            [[7.0], [8.0], [9.0], [10.0], [11.0]],
-            [[8.0], [9.0], [10.0], [11.0], [12.0]],
-            [[9.0], [10.0], [11.0], [12.0], [13.0]],
-            [[10.0], [11.0], [12.0], [13.0], [14.0]],
-        ]
+        [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
     )
     targets = np.array(
-        [[6.0], [7.0], [8.0], [9.0], [10.0], [11.0], [12.0], [13.0], [14.0], [15.0]]
+        [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21]
     )
 
-    hidden_state = np.zeros((hidden_size, 1))
+    # hidden_state = np.zeros((hidden_size, 1))
+    hidden_state = np.zeros((1))
 
     # Forward pass
-    outputs, hidden_states, actual_values = forward_pass(inputs, hidden_state, params)
+    outputs, hidden_states = forward_pass(inputs, hidden_state, params)
 
     print("Input :")
     print(inputs)
@@ -95,8 +86,6 @@ if __name__ == "__main__":
 
     print("\nPredicted :")
     print(outputs)
-    # output의 각 값의 합이 1이 되도록 softmax를 적용했기 때문에, argmax를 통해 가장 큰 값을 가져옴
-    print([actual_values[i][np.argmax(outputs[i])] for i in range(len(outputs))])
 
     # Plot으로 loss 확인
     epoch = np.arange(len(training_loss))
@@ -108,5 +97,5 @@ if __name__ == "__main__":
         label="Training loss",
     )
     plt.legend()
-    plt.xlabel("Epoch"), plt.ylabel("NLL")  # Negative Log Likelihood
+    plt.xlabel("Epoch"), plt.ylabel("RMSE")  # RMSE
     plt.show()
